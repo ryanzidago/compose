28c9a2f0f7fc6baca8a0f882848bdc2c5979fc18
Ryan Zidago <46972947+ryanzidago@users.noreply.github.com>
2024-06-06 22:56:07 +0200

    Refactoring

--- lib/compose/llm.ex
+++ lib/compose/llm.ex
@@ -1,7 +1,105 @@
 defmodule Compose.LLM do
+  alias ComposeWeb.PatientForm
+
   def generate(params) do
     {backend, params} = Map.pop!(params, :backend)
     backend.generate(params)
+  end
+
+  def generate(params, _prompt_mode = :per_form) do
+    system = PatientForm.Prompt.base_prompt()
+
+    input =
+      Jason.encode!(%{
+        locale: params.locale,
+        patient_information: params.patient_report,
+        form: Jason.encode!(PatientForm.schema())
+      })
+
+    generate(%{
+      system: system,
+      input: input,
+      backend: params.backend,
+      model: params.model
+    })
+  end
+
+  def generate(params, _prompt_mode = :per_section) do
+    response =
+      PatientForm.schema()
+      |> Map.keys()
+      |> Map.new(fn section ->
+        encoded_schema =
+          PatientForm.schema()
+          |> Map.get(section)
+          |> Jason.encode!()
+
+        system = PatientForm.related(section).prompt(section)
+
+        input =
+          Jason.encode!(%{
+            locale: Gettext.get_locale(),
+            patient_information: params.patient_report,
+            form: encoded_schema
+          })
+
+        {:ok, response} =
+          generate(%{
+            system: system,
+            input: input,
+            backend: params.backend,
+            model: params.model
+          })
+
+        {Atom.to_string(section), response}
+      end)
+
+    {:ok, response}
+  end
+
+  def generate(params, _prompt_mode = :per_field) do
+    schema = PatientForm.schema()
+
+    response =
+      schema
+      |> Map.keys()
+      |> Map.new(fn section ->
+        fields = Map.get(schema, section)
+
+        response =
+          Enum.reduce(fields, %{}, fn {field, type}, acc ->
+            system = PatientForm.related(section).prompt(section, field)
+
+            input =
+              Jason.encode!(%{
+                locale: Gettext.get_locale(),
+                patient_information: params.patient_report,
+                form: Jason.encode!(%{field => type})
+              })
+
+            {:ok, response} =
+              generate(%{
+                system: system,
+                input: input,
+                backend: params.backend,
+                model: params.model
+              })
+
+            # flatten the response
+            #
+            # sometimes the response is within a `form` key
+            # response = if is_binary(response), do: Jason.decode!(response), else: response
+            # IO.inspect(response)
+            response = Map.get(response, "form", response)
+            response = if is_binary(response), do: Jason.decode!(response), else: response
+
+            Map.merge(acc, response)
+          end)
+
+        {Atom.to_string(section), response}
+      end)
+
+    {:ok, response}
   end
 
   def module(name) when is_atom(name) do
